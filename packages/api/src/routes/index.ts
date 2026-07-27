193b3adffd9439153fbcbe8396bb13c24109e9fb
Wahab Tlais <hussientlais20@gmail.com>
2026-07-28 00:39:56 +0300

    refactor: consolidate contract upload and listing modules

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { authRouter } from "./auth.routes";
 import { userRouter } from "./user.routes";
 import { invitationRouter } from "./invitation.routes";
-import { contractsRouter } from "./contracts.routes";
 import { contractRouter } from "./contract.routes";
 
 const router = Router();
@@ -10,7 +9,6 @@ const router = Router();
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/invitations", invitationRouter);
-router.use("/contracts", contractsRouter);
 router.use("/contracts", contractRouter);
 
 // Add more routers here:
